02077eb628d754a81e7a8a8baeddc5a803bcf4b8
mbucter <mbucter@gmail.com>
2020-07-06 21:35:47 -0700

    transform

--- apt_scraper/extract.py
+++ apt_scraper/extract.py
@@ -1,5 +1,6 @@
 from time import sleep
 from apt_scraper.request_html import RequestHTML
+from apt_scraper.transform import transform
 
 
 BEDROOMS = [
@@ -12,9 +13,10 @@ SITES = [
 ]
 
 def extract():
-    rh = RequestHTML()
-    for site in SITES:
-        for bd in BEDROOMS:
-            rh.get_html(site, bd)
-            rh.save_html(site, bd)
-            sleep(1.5)
+    transform()
+    # rh = RequestHTML()
+    # for site in SITES:
+    #     for bd in BEDROOMS:
+    #         rh.get_html(site, bd)
+    #         rh.save_html(site, bd)
+    #         sleep(1.5)
